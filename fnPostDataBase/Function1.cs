54cfe0576a2bcad43b76f3b68e5ac4308d4e9760
Renato Xavier Pedroso <renatoxpedroso@gmail.com>
2025-01-19 17:01:26 -0300

    Commit postDataBase

---
+++ fnPostDataBase/Function1.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace fnPostDataBase
+{
+    public class Function1
+    {
+        private readonly ILogger<Function1> _logger;
+
+        public Function1(ILogger<Function1> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function("movie")]
+        [CosmosDBOutput("%CosmoDBDatabaseName%", "movies", Connection = "CosmoDBConnection", CreateIfNotExists = true)]
+        public async Task<object?> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            try
+            {
+                MovieRequest movie = null;
+
+                using (StreamReader streamReader = new StreamReader(req.Body))
+                {
+                    string requestBody = await streamReader.ReadToEndAsync();
+                    movie = JsonConvert.DeserializeObject<MovieRequest>(requestBody);
+                }
+
+                return JsonConvert.SerializeObject(movie);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error");
+                return new BadRequestObjectResult("Error");
+            }
+        }
+    }
+}
